abec5c33a0a394c4f785c71ca5aeba019e9f4cf2
Üsame Çetinkaya <142397067+usame-cetinkaya@users.noreply.github.com>
2025-08-16 21:51:34 +0300

    add cron trigger

---
+++ custom-worker.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore `.open-next/worker.ts` is generated at build time
+import { default as handler } from "./.open-next/worker.js";
+
+export default {
+  fetch: handler.fetch,
+
+  /**
+   * Scheduled Handler
+   *
+   * Can be tested with:
+   * - `wrangler dev --test-scheduled`
+   * - `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"`
+   * @param event
+   */
+  async scheduled(event) {
+    console.log("Scheduled event", event);
+  },
+} satisfies ExportedHandler<CloudflareEnv>;
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore `.open-next/worker.ts` is generated at build time
+export { DOQueueHandler, DOShardedTagCache } from "./.open-next/worker.js";
