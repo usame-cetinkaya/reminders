37cb84182419759673f3c789174eaa1808dd1de1
Üsame Çetinkaya <142397067+usame-cetinkaya@users.noreply.github.com>
2025-08-16 22:28:12 +0300

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <pre>{JSON.stringify(session, null, 2)}</pre>
         <Image
           className="dark:invert"
           src="/next.svg"
