f780a400d2d18bf1161d7ff7fc4b92f0dd9c26c0
Üsame Çetinkaya <142397067+usame-cetinkaya@users.noreply.github.com>
2025-08-18 11:55:51 +0300

    list item layout

--- src/components/list-item.tsx
+++ src/components/list-item.tsx
@@ -18,9 +18,9 @@ export default function ListItem({
       onClick={() => onItemClick()}
       className="cursor-pointer flex items-center p-2 pl-4 border rounded-lg"
     >
-      <div className="flex flex-col gap-1 md:flex-row">
+      <div className="flex flex-col gap-1">
         <span className="text-lg font-semibold">{reminder.name}</span>
-        <div className="flex items-center gap-4 md:ml-4 text-sm text-muted-foreground">
+        <div className="flex items-center gap-4 text-sm text-muted-foreground">
           <div className="flex items-center gap-1">
             <Calendar className="size-4" />
             <span>{localDateTime(reminder.remind_at)}</span>
